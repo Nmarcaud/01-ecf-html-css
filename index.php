214da6eb82e5f7bc06b665a3084f3c89e7905211
Nicolas Marcaud <nicolasmarcaud@MacBook-Air-de-Nicolas.local>
2021-06-25 17:25:50 +0200

    Réinitialisation du projet sans Gulp et avec Bootstrap

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php 
+$pageName = 'Home';
+
+@include('parts/head.php');
+
+?>
+<body>
+
+    <?php
+        @include('parts/nav.php');
+    ?>
+
+    <h1>Init</h1>
+
+    <!-- <nav class="main-nav">
+        <button class="toggle-menu" aria-expanded="false" aria-controls="menu">Menu</button>
+        <ul id="menu" class="menu" role="list" hidden>
+            <li class="menu-item">
+                <a href="#" class="menu-link">Link 1</a>
+            </li>
+        </ul>
+    </nav> -->
+
+   
+    
+
+
+    <?php
+        @include('parts/footer.php');
+    ?>
+
+
+    <!-- <script src="js/burger-menu.js"></script> -->
+
+    <!-- Bootsrap -->
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.js"></script>
+    
+</body>
+</html>
---
+++ parts/head.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php if(isset($pageName)) {echo $pageName;}?></title>
+    
+    <!-- Bootsrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Mon style -->
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/helpers.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
