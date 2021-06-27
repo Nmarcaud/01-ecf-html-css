5643859c8557a44bf8456e2481608c4ec289b69e
Nicolas Marcaud <nicolasmarcaud@MacBook-Air-de-Nicolas-2.local>
2021-06-27 20:12:00 +0200

    Page contact terminée

--- contact.php
+++ contact.php
@@ -13,13 +13,60 @@ $pageName = 'Nous contacter';
     @include('parts/nav.php');
 ?>
 
-<main>
+<main class="contact-page-main-content">
+
+   <section>
+
+    <div class="container">
+
+        <!-- Row Title -->
+        <div class="row justify-content-center">
+
+            <div class="col-12 text-center">
+                <h1 class="page-title"><?php echo $pageName; ?></h1>
+            </div>
+
+        </div>  <!-- End - Row Title -->
+
+
+        <!-- Form -->
+        <div class="row justify-content-center pt-5">
+            <div class="col-10">
+
+                <form id="contact-form" class="contact-form" action="#" metod="post">
+                    <div class="row gy-5 g-lg-5">
+                        <div class="col-12 col-lg-6">
+                            <label for="name" class="form-label">Votre nom</label>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Entrez votre nom">
+                        </div>
+
+                        <div class="col-12 col-lg-6">
+                            <label for="email" class="form-label">Votre e-mail</label>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Entrez votre adresse e-mail">
+                        </div>
+
+                        <div class="col-12">
+                            <label for="message" class="form-label">Votre message</label>
+                            <textarea type="text" class="form-control textarea" id="message" name="message" rows="8" placeholder="Tapez votre message"></textarea>
+                        </div>
+
+                        <div class="col-12 d-flex justify-content-end">
+                            <button type="submit" class="btn-orange d-flex align-items-center py-2 px-4 rounded card-footer-btn submit-btn-form">Envoyer<img class="right-arrow-icon" src="img/icon-feather-arrow-right-circle.svg" alt="Icone flèche droite"></button>
+                        </div>
+                    </div>
+           
+                </form>
+
+            </div>
+        </div>
+        
 
 
 
 
 
 
+    </section>
 
 </main>
 
