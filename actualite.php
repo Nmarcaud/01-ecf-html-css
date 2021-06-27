a230908d9296fc705d156f2ac34ee97980def00f
Nicolas Marcaud <nicolasmarcaud@MacBook-Air-de-Nicolas-2.local>
2021-06-27 19:34:53 +0200

    Page étudiant terminée

--- actualite.php
+++ actualite.php
@@ -15,9 +15,8 @@ $pageName = 'Actualité';
 
 <main class="news-page-main-content">
       
-
-    <!-- Texte du module -->
-    <section class="module-text-content">
+    <!-- Section actualité -->
+    <section class="news-text-content">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-12 col-md-10 col-lg-8">
@@ -33,11 +32,11 @@ $pageName = 'Actualité';
                     </small>
                 </div>
                 <div class="col-12 col-md-10 col-lg-8">
-                    <p class="module-text text-section">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis. Iaculis urna id volutpat lacus laoreet. Mauris vitae ultricies leo integer malesuada. Ac odio tempor orci dapibus ultrices in. Egestas diam in arcu cursus euismod. Dictum fusce ut placerat orci nulla. Tincidunt ornare massa eget egestas purus viverra accumsan in nisl. Tempor id eu nisl nunc mi ipsum faucibus. Fusce id velit ut tortor pretium. Massa ultricies mi quis hendrerit dolor magna eget. Nullam eget felis eget nunc lobortis. Faucibus ornare suspendisse sed nisi. Sagittis eu volutpat odio facilisis mauris sit amet massa. Erat velit scelerisque in dictum non consectetur a erat. Amet nulla facilisi morbi tempus iaculis urna. Egestas purus viverra accumsan in nisl. Feugiat in ante metus dictum at tempor commodo. Convallis tellus id interdum velit laoreet. Proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Viverra aliquet eget sit amet tellus cras adipiscing enim eu. Ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. Dui faucibus in ornare quam. In iaculis nunc sed augue lacus viverra vitae congue. Vitae tempus quam pellentesque nec nam aliquam sem et. Ut morbi tincidunt augue interdum. Sem fringilla ut morbi tincidunt augue. Morbi enim nunc faucibus a pellentesque sit amet porttitor eget. In est ante in nibh mauris. Nam aliquam sem et tortor consequat id porta nibh. Diam quis enim lobortis scelerisque fermentum dui faucibus. Non curabitur gravida arcu ac. Magna fringilla urna porttitor rhoncus dolor. Aenean et tortor at risus viverra adipiscing. Dignissim sodales ut eu sem. Quam quisque id diam vel quam elementum pulvinar etiam non. Eleifend quam adipiscing vitae proin sagittis. Enim facilisis gravida neque convallis a cras semper. Risus commodo viverra maecenas accumsan. Sit amet porttitor eget dolor morbi non arcu risus. Vitae et leo duis ut diam quam. Aliquam faucibus purus in massa tempor. Quisque egestas diam in arcu cursus. Nibh sit amet commodo nulla facilisi</p>
+                    <p class="news-text text-section">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis. Iaculis urna id volutpat lacus laoreet. Mauris vitae ultricies leo integer malesuada. Ac odio tempor orci dapibus ultrices in. Egestas diam in arcu cursus euismod. Dictum fusce ut placerat orci nulla. Tincidunt ornare massa eget egestas purus viverra accumsan in nisl. Tempor id eu nisl nunc mi ipsum faucibus. Fusce id velit ut tortor pretium. Massa ultricies mi quis hendrerit dolor magna eget. Nullam eget felis eget nunc lobortis. Faucibus ornare suspendisse sed nisi. Sagittis eu volutpat odio facilisis mauris sit amet massa. Erat velit scelerisque in dictum non consectetur a erat. Amet nulla facilisi morbi tempus iaculis urna. Egestas purus viverra accumsan in nisl. Feugiat in ante metus dictum at tempor commodo. Convallis tellus id interdum velit laoreet. Proin sagittis nisl rhoncus mattis rhoncus urna neque viverra. Viverra aliquet eget sit amet tellus cras adipiscing enim eu. Ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. Dui faucibus in ornare quam. In iaculis nunc sed augue lacus viverra vitae congue. Vitae tempus quam pellentesque nec nam aliquam sem et. Ut morbi tincidunt augue interdum. Sem fringilla ut morbi tincidunt augue. Morbi enim nunc faucibus a pellentesque sit amet porttitor eget. In est ante in nibh mauris. Nam aliquam sem et tortor consequat id porta nibh. Diam quis enim lobortis scelerisque fermentum dui faucibus. Non curabitur gravida arcu ac. Magna fringilla urna porttitor rhoncus dolor. Aenean et tortor at risus viverra adipiscing. Dignissim sodales ut eu sem. Quam quisque id diam vel quam elementum pulvinar etiam non. Eleifend quam adipiscing vitae proin sagittis. Enim facilisis gravida neque convallis a cras semper. Risus commodo viverra maecenas accumsan. Sit amet porttitor eget dolor morbi non arcu risus. Vitae et leo duis ut diam quam. Aliquam faucibus purus in massa tempor. Quisque egestas diam in arcu cursus. Nibh sit amet commodo nulla facilisi</p>
                 </div>
             </div>
-        </div>
-    </section>  <!-- End - Texte du module -->
+        </div>  <!-- End - Container -->
+    </section>  <!-- End - Section actualité -->
 
 
 
--- etudiant.php
+++ etudiant.php
@@ -13,13 +13,63 @@ $pageName = 'Étudiant';
     @include('parts/nav.php');
 ?>
 
-<main>
+<main class="student-page-main-content">
 
+    <!-- Section student -->
+    <section class="student-text-content">
+        <div class="container">
 
+            <div class="row justify-content-center">
+                <div class="col-12 d-flex justify-content-center">
+                    <img class="student-img-page" src="img/francine_min.webp" srcset="img/francine@2x_min.webp 2x" alt="Portrait de Francine Auhi">
+                </div>
+                <div class="col-12 col-md-10 col-lg-8 text-center">
+                    <h1 class="page-title">Francine Auhi</h1>
+                </div>
+            </div>
 
+            <ul class="features-list row gy-5 g-md-5" role="list">
+                <li class="feature-item col-12 col-md-6 col-xl-3">
+                    <h2 class="feature-title">Son film</h2>
+                    <p class="feature-text">Lorem ipsum dolor sit</p>
+                </li>
+                <li class="feature-item col-12 col-md-6 col-xl-3">
+                    <h2 class="feature-title">Sa série</h2>
+                    <p class="feature-text">Lorem ipsum dolor sit</p>
+                </li>
+                <li class="feature-item col-12 col-md-6 col-xl-3">
+                    <h2 class="feature-title">Son jeu vidéo</h2>
+                    <p class="feature-text">Lorem ipsum dolor sit</p>
+                </li>
+                <li class="feature-item col-12 col-md-6 col-xl-3">
+                    <h2 class="feature-title">Son héros/héroïne</h2>
+                    <p class="feature-text">Lorem ipsum dolor sit</p>
+                </li>
+                <li class="feature-item col-12 col-md-6 col-xl-3">
+                    <h2 class="feature-title">Son livre</h2>
+                    <p class="feature-text">Lorem ipsum dolor sit</p>
+                </li>
+                <li class="feature-item col-12 col-md-6 col-xl-3">
+                    <h2 class="feature-title">Sa chanson</h2>
+                    <p class="feature-text">Lorem ipsum dolor sit</p>
+                </li>
+                <li class="feature-item col-12 col-md-6 col-xl-3">
+                    <h2 class="feature-title">Son application</h2>
+                    <p class="feature-text">Lorem ipsum dolor sit</p>
+                </li>
+                <li class="feature-item col-12 col-md-6 col-xl-3">
+                    <h2 class="feature-title">Son site web</h2>
+                    <p class="feature-text">Lorem ipsum dolor sit</p>
+                </li>
+                <li class="feature-item col-12">
+                    <h2 class="feature-title">En deux mots</h2>
+                    <p class="feature-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis. Iaculis urna id volutpat lacus laoreet.</p>
+                </li>
+            </ul>
 
+        </div>  <!-- End - Container -->
 
-
+    </section>  <!-- End - Section student -->
 
 </main>
 
