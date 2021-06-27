a5e7f2a3f3abe9c690b45942f3951bb8e97d0704
Nicolas Marcaud <nicolasmarcaud@MacBook-Air-de-Nicolas.local>
2021-06-27 18:36:44 +0200

    Page module de formation terminée

--- module.php
+++ module.php
@@ -13,9 +13,30 @@ $pageName = 'Module';
     @include('parts/nav.php');
 ?>
 
-<main>
+<main class="module-page-main-content">
+      
+    <!-- Hero du Module de formation -->
+    <section class="module-hero">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    <h1 class="page-title">Module HTML / CSS</h1>
+                </div>
+            </div>
+        </div>
 
+    </section>  <!-- End - Texte du module -->
 
+    <!-- Texte du module -->
+    <section class="module-text-content">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-10 col-lg-8">
+                    <p class="module-text text-section">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Bibendum est ultricies integer quis. Iaculis urna id volutpat lacus laoreet. Mauris vitae ultricies leo integer malesuada. Ac odio tempor orci dapibus ultrices in. Egestas diam in arcu cursus euismod. Dictum fusce ut placerat orci nulla. Tincidunt ornare massa eget egestas purus viverra accumsan in nisl. Tempor id eu nisl nunc mi ipsum faucibus. Fusce id velit ut tortor pretium. Massa ultricies mi quis hendrerit dolor magna eget. Nullam eget felis eget nunc lobortis. Faucibus ornare suspendisse sed nisi. Sagittis eu volutpat odio facilisis mauris sit amet massa. Erat velit scelerisque in dictum non consectetur a erat. Amet nulla facilisi morbi tempus iaculis urna. Egestas</p>
+                </div>
+            </div>
+        </div>
+    </section>  <!-- End - Texte du module -->
 
 
 
